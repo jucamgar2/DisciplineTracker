31bd7eb85353ab6a18d98ec55db0168b765ffcd9
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2026-02-20 14:47:54 +0100

    Backend initialization

---
+++ disciplinetracker/src/main/java/backend/disciplinetracker/DisciplinetrackerApplication.java
@@ -0,0 +1,13 @@
+package backend.disciplinetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinetrackerApplication.class, args);
+	}
+
+}
