5f07291cb789bb441db0dd2ea32d64072edc9dc1
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2026-04-06 18:53:13 +0200

    fe-ft: Initial frontend configuration

--- disciplinetracker-frontend/src/App.jsx
+++ disciplinetracker-frontend/src/App.jsx
@@ -2,7 +2,30 @@ import './App.css'
 
 function App() {
 
-  return ;
+  let date = new Date();
+  let year = date.getFullYear();
+  let day = date.getDay();
+  let dayName = date.toLocaleDateString("ES-es", {weekday: 'long'});
+  dayName = dayName.charAt(0).toUpperCase() + dayName.slice(1);
+  let monthName = date.toLocaleDateString("ES-es", {month:'long'})
+  monthName = monthName.charAt(0).toUpperCase() + monthName.slice(1);
+  console.log(monthName);
+
+
+  return (
+    <div>
+      <h1 className='text-2xl'>{dayName}, {day} de {monthName} de {year} </h1>
+      <ul>
+        <li>Gimnasio</li>
+      </ul>
+      <h2>ESTADO SEMANAL</h2>
+      <div>
+
+      </div>
+
+    </div>
+
+  );
 }
 
 export default App
---
+++ disciplinetracker-frontend/src/pages/user/Register.jsx
@@ -0,0 +1,20 @@
+function Register() {
+
+    return(
+        <div>
+            <h1>Discipline Tracker</h1>
+            <h3>Nombre de usuario</h3>
+            <input></input>
+            <h3>Contraseña</h3>
+            <input></input>
+            <h3>Nombre</h3>
+            <input></input>
+            <h3>Apellido</h3>
+            <input></input>
+            <h3>Fecha de nacimiento</h3>
+            <input></input>
+        </div>
+    )
+}
+
+export default Register;
